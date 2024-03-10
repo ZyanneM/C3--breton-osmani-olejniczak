ffbdcf78ba5a9b96526fad2a8ee5305406b93cc9
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-03-10 09:16:05 +0100

    Call the generateTeams function from TeamGenerator class

---
+++ test/teamGenerator.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import TeamGenerator from '../src/teamGenerator';
+
+describe('TeamGenerator', () => {
+  it('Devrait générer les équipes avec le bon nombre de joueurs dans chaque équipe', () => {
+    const players = ['Joueur 1', 'Joueur 2', 'Joueur 3', 'Joueur 4', 'Joueur 5', 'Joueur 6'];
+    const playersPerTeam = 3;
+    const teamGenerator = new TeamGenerator(players, playersPerTeam);
+    teamGenerator.generateTeams();
+  });
+});
