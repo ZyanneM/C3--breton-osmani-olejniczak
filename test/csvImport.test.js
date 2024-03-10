481e59c51469b27db45e2ac73bdb232b056ca4f3
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-03-10 12:14:46 +0100

    Replace mock by dynamic data

--- test/csvImport.test.js
+++ test/csvImport.test.js
@@ -7,25 +7,12 @@ import { expect } from 'chai';
 describe('CSVReader', () => {
   describe('readCSV', () => {
     it("devrait lire le fichier CSV et retourner les données sous forme d'un tableau d'objets", (done) => {
-        const testFilePath = fileURLToPath(import.meta.url);
-        const testDirPath = path.dirname(testFilePath);
-        const filePath = path.resolve(testDirPath, '../src/test.csv');
+      const testFilePath = fileURLToPath(import.meta.url);
+      const testDirPath = path.dirname(testFilePath);
+      const filePath = path.resolve(testDirPath, '../src/test.csv');
       const csvReader = new CSVReader(filePath);
 
-      const expectedPlayers = [
-        { nom: 'Uzumaki', prenom: 'Naruto' },
-        { nom: 'Uchiha', prenom: 'Sasuke' },
-        { nom: 'Haruno', prenom: 'Sakura' },
-        { nom: 'Hatake', prenom: 'Kakashi' },
-        { nom: 'Nara', prenom: 'Shikamaru' },
-        { nom: 'Hyuga', prenom: 'Hinata' },
-        { nom: 'Hyuga', prenom: 'Neji' },
-        { nom: 'Lee', prenom: 'Rock' },
-        { nom: 'Tenten', prenom: '' },
-        { nom: 'Yamanaka', prenom: 'Ino' },
-        { nom: 'Akimichi', prenom: 'Choji' },
-        { nom: 'Inuzuka', prenom: 'Kiba' }
-      ];
+      const expectedPlayers = [];
 
       function logData(data) {
         console.log("Données lues à partir du fichier CSV :");
@@ -33,16 +20,31 @@ describe('CSVReader', () => {
       }
 
       csvReader.readCSV()
-      .then((data) => {
-        logData(data);
-        expect(data).to.be.an('array').that.is.not.empty;
-        data.forEach(item => expect(item).to.be.an('object'));
-        data.forEach((player, index) => {
-            expect(player).to.deep.equal(expectedPlayers[index]);
+        .then((data) => {
+          logData(data);
+          expect(data).to.be.an('array').that.is.not.empty;
+          data.forEach(item => expect(item).to.be.an('object'));
+          data.forEach((player) => {
+            const playerKeys = Object.keys(player);
+            const playerData = {};
+
+            playerKeys.forEach((key) => {
+              if (key.toLowerCase() === 'nom' || key.toLowerCase() === 'prenom') {
+                playerData[key.toLowerCase()] = player[key];
+              }
+            });
+
+            expectedPlayers.push(playerData);
           });
-        done();
-      })
-      .catch(done);
+
+          expectedPlayers.forEach((expectedPlayer, index) => {
+            expect(data[index].nom).to.equal(expectedPlayer.nom);
+            expect(data[index].prenom).to.equal(expectedPlayer.prenom);
+          });
+
+          done();
+        })
+        .catch(done);
+    });
   });
-});
 });
