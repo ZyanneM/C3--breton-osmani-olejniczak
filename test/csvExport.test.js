d8c55da91ee26c3abd6f966e471555802a4605eb
Zoe <osmani.zoe34@gmail.com>
2024-03-10 15:48:47 +0100

    modif test pour vider le dossier temps à chaque test pour ne pas à le faire manuellement

--- test/csvExport.test.js
+++ test/csvExport.test.js
@@ -1,20 +1,33 @@
 import { expect } from 'chai';
 import {saveCSVtoTempFolder} from "../src/csvExport.js";
-
+import fs from "fs";
+import path from "path";
 describe('CSV Exporter', () => {
-    it('devrait enregistrer le fichier CSV dans le dossier temporaire', async () => {
-        const data = [
+    let data;
+    let fileName;
+
+    beforeEach(function () {
+        data = [
             { name: 'Équipe 1', players: ['Joueur 1', 'Joueur 2', 'Joueur 3'] },
             { name: 'Équipe 2', players: ['Joueur 4', 'Joueur 5', 'Joueur 6'] },
             { name: 'Équipe 3', players: ['Joueur 7', 'Joueur 8', 'Joueur 9'] },
         ];
+        fileName = 'test_data';
+    });
 
-        const fileName = 'test_data';
+    afterEach(() => {
+        // Vider le dossier temporaire
+        let tempFolderPath = './temp';
+        fs.readdirSync(tempFolderPath).forEach(file => {
+            const filePath = path.join(tempFolderPath, file);
+            fs.unlinkSync(filePath);
+        });
+    });
+    it('devrait enregistrer le fichier CSV dans le dossier temporaire', async () => {
         const filePath = await saveCSVtoTempFolder(data, fileName);
 
         // Vérifier si le fichier existe
         expect(filePath).to.not.be.null;
         expect(filePath).to.not.be.undefined;
-
     });
 });
