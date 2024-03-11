a902fc2d3f1b14f24797758bd0283fe21c2c0946
Roxane Breton <113140527+roxane-julie@users.noreply.github.com>
2024-03-11 10:26:09 +0100

    Update teamGenerator.test.js
    séparation en deux tests aux taches spécifiques

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -26,11 +26,31 @@ describe("TeamGenerator", () => {
     const teams = teamGenerator.getTeams();
 
     expect(teams).to.be.an("array");
-    expect(teams.length).to.equal(Math.ceil(players.length / playersPerTeam));
 
     teams.forEach((team) => {
       expect(team.players).to.be.an("array");
       expect(team.players.length).to.equal(playersPerTeam);
     });
   });
+
+  it("Devrait avoir le bon typage des données dans TeamGenerator", () => {
+    // Préparation des données de test
+    const players = [
+      "Joueur 1",
+      "Joueur 2",
+      "Joueur 3",
+      "Joueur 4",
+      "Joueur 5",
+      "Joueur 6",
+    ];
+    const playersPerTeam = 3;
+
+    const teamGenerator = new TeamGenerator(players, playersPerTeam);
+
+    expect(teamGenerator.players).to.be.an("array");
+    expect(teamGenerator.players).to.not.be.empty;
+    expect(playersPerTeam).to.be.a("number");
+    expect(playersPerTeam).to.be.above(0);
+  });
+
 });
